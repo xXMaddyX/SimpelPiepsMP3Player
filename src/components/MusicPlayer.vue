61fac891c51aa92e59bd9b21ec0ca0a704a0dc64
Sven Höhn <126699113+xXMaddyX@users.noreply.github.com>
2023-10-10 14:08:16 +0200

    Add files via upload

---
+++ src/components/MusicPlayer.vue
@@ -0,0 +1,67 @@
+<script setup>
+import { ref, inject } from 'vue';
+
+const audioPlayer = ref(null);
+const currentTrackIndex = ref(0);
+const tracklist = inject('musicFiles');
+const currentTrackName = ref('');
+
+const playTrack = (trackPath, index) => {
+  audioPlayer.value.src = trackPath;
+  audioPlayer.value.play();
+  currentTrackIndex.value = index;
+  currentTrackName.value = tracklist.value[index];
+};
+
+const playNextTrack = () => {
+  if (currentTrackIndex.value + 1 < tracklist.value.length) {
+    currentTrackIndex.value++;
+    playTrack(tracklist.value[currentTrackIndex.value], currentTrackIndex.value);
+  }
+};
+
+defineExpose({ playTrack, playNextTrack })
+</script>
+
+<template>
+    <main class="fixed-player">
+        <h3>{{ currentTrackName }}</h3>
+        <img src="../assets/PiepsmitTasse.png" alt="">
+        <audio ref="audioPlayer" controls @ended="playNextTrack"></audio>
+    </main>
+</template>
+
+
+
+<style scoped>
+main {
+  position: relative;
+  display: flex;
+  flex-direction: column;
+  width: 100%;
+  justify-content: center;
+  align-items: center;
+
+}
+
+main audio {
+  position: fixed;
+    width: 50%;
+    bottom: 0px;
+}
+
+main h3 {
+    color: white;
+    text-shadow: 2px 2px 8px white;
+    padding: 20px;
+    padding-bottom: 50px;
+}
+
+main img {
+  height: auto;
+  width: auto;
+  max-height: 50vh;
+  padding-bottom: 100px;
+}
+
+</style>
---
+++ src/components/TrackList.vue
@@ -0,0 +1,71 @@
+<script setup>
+import { inject } from 'vue';
+
+const tracklist = inject('musicFiles');
+const emit = defineEmits(['trackSelected']); 
+
+const selectTrack = (track, index) => {
+  emit('trackSelected', track, index);
+};
+</script>
+
+<template>
+    <div>
+        <ul>
+          <li v-for="(track, index) in tracklist" :key="index" @click="selectTrack(track, index)">
+              {{ track }}
+          </li>
+        </ul>
+    </div>
+</template>
+
+
+
+<style scoped>
+div {
+    position: relative;
+    overflow: scroll;
+    background-color: orange;
+    width: auto;
+    height: 100vh;
+    padding-bottom: 58px;
+}
+
+div ul {
+    list-style: none;
+}
+
+div ul li {
+    padding: 10px;
+    font-weight: 600;
+    cursor: pointer;
+    transition: .3s;
+}
+
+div ul li:hover {
+    background-color: black;
+    color: white;
+    text-shadow: 0px 2px 8px white;
+}
+
+::-webkit-scrollbar {
+    width: 5px; /* Chrome, Safari, Opera */
+    height: 0px;
+  }
+  
+  /* Farbe der Scrollbar */
+  ::-webkit-scrollbar-thumb {
+    background-color: #888888d0; /* Farbe des Scrollbalkens */
+    border-radius: 20px;
+  }
+  
+  /* Farbe bei Mouse-Over */
+  ::-webkit-scrollbar-thumb:hover {
+    background-color: #555555; /* Farbe des Scrollbalkens bei Mouse-Over */
+  }
+  
+  /* Hintergrundfarbe der Scrollbar */
+  ::-webkit-scrollbar-track {
+    background-color: #3a3a3a00; /* Hintergrundfarbe der Scrollbar */
+  }
+</style>
